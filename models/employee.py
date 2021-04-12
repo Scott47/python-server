b16b293546a77c8dad09b45843b5649b5e91c666
Scott47 <scottsilver47@gmail.com>
2021-04-12 12:00:41 -0500

    added db connection and queries for get_all() and get_one()

--- models/employee.py
+++ models/employee.py
@@ -5,8 +5,8 @@ class Employee():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, first_name, last_name, location_id):
+    def __init__(self, id, name, address, location_id):
         self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
+        self.name = name
+        self.address = address
         self.location_id = location_id
