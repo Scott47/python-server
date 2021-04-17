9899d79add8328f74c5fa8f1507afbafd77cd59e
Scott47 <scottsilver47@gmail.com>
2021-04-17 11:01:07 -0500

    added sql join to get_all_animals() to provide client with related data in JSON response for a resource

--- animals/request.py
+++ animals/request.py
@@ -1,4 +1,5 @@
-from models import Animal
+from models import (Animal,
+                    Location)
 import sqlite3
 import json
 
@@ -18,8 +19,12 @@ def get_all_animals():
             a.breed,
             a.status,
             a.location_id,
-            a.customer_id
-        FROM animal a
+            a.customer_id,
+            l.name location_name,
+            l.address location_address
+        FROM Animal a
+        JOIN Location l
+            ON l.id = a.location_id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -38,6 +43,12 @@ def get_all_animals():
             animal = Animal(row['id'], row['name'], row['breed'],
                             row['status'], row['location_id'],
                             row['customer_id'])
+
+            # Create a Location instance from the current row
+            location = Location(row['id'], row['location_name'], row['location_address'])
+
+            # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
 
             animals.append(animal.__dict__)
 
